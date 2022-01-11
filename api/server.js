6314c8720002d10bd25a6b1a3a0e3c744a8b562d
Michael Orvin <morvin1212@gmail.com>
2022-01-10 19:56:19 -0700

    Completed the Delete portion of the MVP

--- api/server.js
+++ api/server.js
@@ -1,7 +1,41 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
 const User = require('./users/model');
+
 const server = express()
+server.use(express.json())
+
+server.delete('/api/users/:id', async (req, res) => {
+    const pUser = await User.findById(req.params.id)
+    if (!pUser) {
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist',
+        })
+    } else {
+        const dUser = await User.remove(pUser.id)
+        res.status(200).json(dUser)
+    }
+})
+
+server.post('./api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user',
+        })
+    } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error creating users',
+            error: err.message,
+        })
+    })
+}
+})
 
 
 server.get('/api/users', (req, res) => {
