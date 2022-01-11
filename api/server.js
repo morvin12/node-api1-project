f5e21bd97b3169f1cf1bb264f09a2e40460e9e62
Michael Orvin <morvin1212@gmail.com>
2022-01-10 17:36:47 -0700

    received response from the server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,14 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const res = require('express/lib/response');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use('*', (req, res) =>{
+    res.status(404).json({
+        message: 'not found'
+    })
+  })
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
